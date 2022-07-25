c8d7d57fcbee83cb596ef9a9a442dbba46b48b04
bit-shashank <shashankcs083@gmail.com>
2022-07-25 13:22:32 +0530

    Added in memory database

--- CalculateNetWorth/src/main/java/com/networth/config/SecurityConfig.java
+++ CalculateNetWorth/src/main/java/com/networth/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
+        http.csrf().disable().authorizeRequests().antMatchers("/authenticate","/stocks","/h2-console/**")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
